dcee8611bbc9f0cba879ae82aa5489e58017b240
niconickniner <18696833750@163.com>
2016-03-10 15:34:14 +0800

    changed the main act, added intent jump into first act.

--- Survey/src/main/java/com/cqu/tangdwx/surveystar/welComeActivity.java
+++ Survey/src/main/java/com/cqu/tangdwx/surveystar/welComeActivity.java
@@ -1,7 +1,9 @@
 package com.cqu.tangdwx.surveystar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class welComeActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class welComeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wel_come);
+    }
+    public void goStart(View v) {
+        Intent it = new Intent(this,MainActivity.class);
+        startActivity(it);
     }
 }
